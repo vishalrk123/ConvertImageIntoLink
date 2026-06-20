e0a9134713de7a3187d2dcc20d9ef831c23c8aab
Vishal Kumar <133629896+vishalrk123@users.noreply.github.com>
2026-06-20 21:23:22 +0530

    Update API base URL for axios instance

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://foreverlink-backend.onrender.com/api",
+  baseURL: "https://foreverlink-s6lz.onrender.com/api",
 });
 
 export default API;
